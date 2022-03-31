383d936dabc2b0541d597a6654866ec6d689e6a9
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-03-31 16:32:35 +0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -3,31 +3,31 @@ require_relative './enumerable'
 class MyList
   include MyEnumerable
   def initialize(*list)
-   @list = list
+    @list = list
   end
 
   def each(&block)
-   @list.each(&block)
+    @list.each(&block)
   end
 end
 
 # verify solution
 # Create our list
-irb> list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 3, 4)
 # => <MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
-irb> list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1, 2, 3, 4)
 # => true
-irb> list.my_all? {|e| e > 5}
+list.my_all? { |e| e > 5 }
 # => false
 
 # Test #any?
-irb> list.my_any? {|e| e == 2}
+list.my_any? { |e| e == 2 }
 # => true
-irb> list.my_any? {|e| e == 5}
+list.my_any? { |e| e == 5 }
 # => false
 
 # Test #filter
-irb> list.my_filter? {|e| e.even?}
+list.my_filter? { |e| e.even? }
 # => [2, 4]
