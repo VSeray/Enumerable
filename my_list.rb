de6f5d2ba70a8a033707ecc4baf84bdf88641176
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-03-31 16:57:24 +0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -11,20 +11,3 @@ class MyList
   end
 end
 
-# verify solution
-
-# Test #all?
-list = MyList.new(1, 2, 3, 4)
-# => true
-list.my_all? { |e| e > 5 }
-# => false
-
-# Test #any?
-list.my_any? { |e| e == 2 }
-# => true
-list.my_any? { |e| e == 5 }
-# => false
-
-# Test #filter
-list.my_filter? (&:even?)
-# => [2, 4]
