4e58c2a3d12cf59e8af2bd343e3e431ae36583e6
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-03-31 16:14:51 +0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -14,20 +14,20 @@ end
 #verify solution
 # Create our list
 irb> list = MyList.new(1, 2, 3, 4)
-#<MyList: @list=[1, 2, 3, 4]>
+=> #<MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
 irb> list = MyList.new(1, 2, 3, 4)
-true
+=> true
 irb> list.my_all? {|e| e > 5}
-false
+=> false
 
 # Test #any?
 irb> list.my_any? {|e| e == 2}
-true
+=> true
 irb> list.my_any? {|e| e == 5}
-false
+=> false
 
 # Test #filter
 irb> list.my_filter? {|e| e.even?}
-[2, 4]
+=> [2, 4]
