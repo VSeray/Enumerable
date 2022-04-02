13e2a3b37587ffd87a287ed05c324f8c94c20892
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-04-02 13:07:58 +0300

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -10,3 +10,23 @@ class MyList
     @list.each(&block)
   end
 end
+
+# Create our list
+irb> list = MyList.new(1, 2, 3, 4)
+=> #<MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+irb> list.my_all? {|e| e < 5}
+=> true
+irb> list.my_all? {|e| e > 5}
+=> false
+
+# Test #any?
+irb> list.my_any? {|e| e == 2}
+=> true
+irb> list.my_any? {|e| e == 5}
+=> false
+
+# Test #filter
+irb> list.my_filter? {|e| e.even?}
+=> [2, 4]
